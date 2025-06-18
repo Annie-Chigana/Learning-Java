af1eaf728aed5aa5c791b982fb68c6ce0386d53b
Annie-Chigana <chiganagana.a@gmail.com>
2025-06-18 10:13:54 +0200

    Confirm Dialog boxes

--- SalaryDialog/src/salarydialog/SalaryDialog.java
+++ SalaryDialog/src/salarydialog/SalaryDialog.java
@@ -14,12 +14,19 @@ public class SalaryDialog {
         int dependents;
         final double HOURS_IN_WEEK = 37.5;
         
+        int selection;
+        boolean isYes;
+        
         wageString = JOptionPane.showInputDialog(null, "Enter employee's hourly wage", "Salary dialog 1", JOptionPane.INFORMATION_MESSAGE);
         weeklyPay = Double.parseDouble(wageString) * HOURS_IN_WEEK;
         dependentsString = JOptionPane.showInputDialog(null, "How many dependents?", "Salary Dialog 2", JOptionPane.QUESTION_MESSAGE);
         dependents = Integer.parseInt(dependentsString);
         
-        JOptionPane.showMessageDialog(null, "Weekly salary is $" + weeklyPay + "\nDeductions will be made for " + dependents + " dependents");
+        
+        selection = JOptionPane.showConfirmDialog(null, "Confirm that you work 37.5 hours in a week");
+        
+        isYes = (selection == JOptionPane.YES_OPTION);
+        JOptionPane.showMessageDialog(null, isYes + "Weekly salary is $" + weeklyPay + "\nDeductions will be made for " + dependents + " dependents");
     }
     
 }
