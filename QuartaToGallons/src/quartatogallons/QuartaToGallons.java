5cfd9637ad51af39e3427ac156c98dc8cb5c9450
Annie-Chigana <chiganagana.a@gmail.com>
2025-06-22 20:36:38 +0200

    Quarts to Gallons interactive

--- QuartaToGallons/src/quartatogallons/QuartaToGallons.java
+++ QuartaToGallons/src/quartatogallons/QuartaToGallons.java
@@ -1,20 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//Filename QuartsToGallons.Java
+//Written by Annie
+//Written on 22/06/2025
 package quartatogallons;
 
-/**
- *
- * @author annie
- */
+import java.util.Scanner;
+
 public class QuartaToGallons {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        float quartsInGallon = 4;
+        //float quartsNeeded = 18;
+        
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter the number of quarts needed: ");
+        float quartsNeeded = userInput.nextFloat();
+        
+        float gallons;
+        
+        gallons = quartsNeeded/quartsInGallon;
+        
+        System.out.println("A job that needs " + quartsNeeded + " quarts requires " + gallons + " gallons");
+        
+        
     }
     
 }
