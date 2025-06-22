2b90e8e08736fa9180c3916bb09822ef00c5ab8b
Annie-Chigana <chiganagana.a@gmail.com>
2025-06-22 20:21:51 +0200

    random game first part

---
+++ RandomNumbers/src/randomnumbers/RandomNumbers.java
@@ -0,0 +1,28 @@
+//Filename RandomNumbers.Java
+//Written by Annie
+//Written on 22/06/2025
+package randomnumbers;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class RandomNumbers {
+
+    public static void main(String[] args) {
+        
+        int userNum;
+        int Min = 1;
+        int Max = 5;
+        
+        int random = Min + (int)(Math.random()*Max);
+        
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter a random number between 1 to 5: ");
+        
+        userNum = userInput.nextInt();
+        
+        System.out.println("The random number is: " + random + " You entered " + userNum);
+        
+    }
+    
+}
