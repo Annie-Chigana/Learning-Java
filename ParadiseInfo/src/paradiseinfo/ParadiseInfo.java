d01821800ee404a15a30de6f55857a530bd0cf29
Annie-Chigana <chiganagana.a@gmail.com>
2025-06-24 14:30:41 +0200

    First Method project to display information that outside of the main method, in another method called display info

---
+++ ParadiseInfo/src/paradiseinfo/ParadiseInfo.java
@@ -0,0 +1,19 @@
+//Filename ParadiseInfo.Java
+//Written by Annie
+//Written on 24/06/2025
+package paradiseinfo;
+
+public class ParadiseInfo {
+
+    public static void main(String[] args) {
+       
+        displayInfo();
+    }
+    
+    public static void displayInfo()
+    {
+        System.out.println("Paradise Day Spa wants to pamper you.");
+        System.out.println("We will make you look good.");
+    }
+    
+}
